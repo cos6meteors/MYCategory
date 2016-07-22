346f3eb5b961063a8a49ad06c1868cb9f96a2784
Murphy Zheng <murphy@mieasy.com>
2016-07-22 14:46:15 +0800

    Add a category to post files

---
+++ URLRequestPostFile/NSMutableURLRequest+PostFile.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableURLRequest+PostFile.h
+//  OfficialDemo3D
+//
+//  Created by Murphy Zheng on 16/7/22.
+//  Copyright © 2016年 songjian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableURLRequest (PostFile)
+
+@end
