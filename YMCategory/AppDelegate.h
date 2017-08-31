9578498224851f9973baca4b4ac641edea95fb0c
Murphy Zheng <murphy@mieasy.com>
2017-08-31 14:47:32 +0800

    Create a single project

---
+++ YMCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YMCategory
+//
+//  Created by Murphy Zheng on 17/8/31.
+//  Copyright © 2017年 mieasy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
