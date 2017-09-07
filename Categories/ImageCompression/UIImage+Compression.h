d0a03016aa2aeba3a1c24771d3bd8f778b4abe75
Murphy Zheng <murphy@mieasy.com>
2017-09-07 16:19:29 +0800

    Accomplish the category of Image Compression

--- Categories/ImageCompression/UIImage+Compression.h
+++ Categories/ImageCompression/UIImage+Compression.h
@@ -10,4 +10,34 @@
 
 @interface UIImage (Compression)
 
+
+/**
+ 压缩图片质量（图片体积减小，像素不变）
+
+ @param image origin image
+ @param percent compress percent
+ @return compressed image
+ */
++ (UIImage *)reduceImage:(UIImage *)image percent:(float)percent;
+
+
+/**
+图片压缩到指定尺寸
+
+ @param image origin image
+ @param newSize to size
+ @return compressed image
+ */
++ (UIImage *)scaleImage:(UIImage *)image withNewSize:(CGSize)newSize;
+
+
+/**
+ 等比例压缩（压缩到指定尺寸）
+
+ @param image origin image
+ @param targetSize target image size
+ @return compressed image
+ */
++ (NSData *)compressImage:(UIImage *)image targetSize:(CGSize)targetSize;
+
 @end
--- Categories/ImageCompression/UIImage+Compression.m
+++ Categories/ImageCompression/UIImage+Compression.m
@@ -10,4 +10,62 @@
 
 @implementation UIImage (Compression)
 
++ (UIImage *)reduceImage:(UIImage *)image percent:(float)percent {
+    NSData *imageData = UIImageJPEGRepresentation(image, percent);
+    UIImage *newImage = [UIImage imageWithData:imageData];
+    return newImage;
+}
+
++ (UIImage *)scaleImage:(UIImage *)image withNewSize:(CGSize)newSize {
+    UIGraphicsBeginImageContext(newSize);
+    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
++ (NSData *)compressImage:(UIImage *)image targetSize:(CGSize)targetSize {
+    UIImage *newImage = nil;
+    CGSize imageSize = image.size;
+    CGFloat width  = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth  = targetSize.width;
+    CGFloat targetHeight = targetSize.height;
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth  = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0, 0.0);
+    if (CGSizeEqualToSize(imageSize, targetSize)) {
+        CGFloat widthFactor = targetWidth/width;
+        CGFloat heightFactor = targetHeight/height;
+        if(widthFactor > heightFactor){
+            scaleFactor = widthFactor;
+        }
+        else{
+            scaleFactor = heightFactor;
+        }
+        scaledWidth = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        if(widthFactor > heightFactor){
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+        }else if(widthFactor < heightFactor){
+            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+        }
+    }
+    UIGraphicsBeginImageContext(targetSize);
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    [image drawInRect:thumbnailRect];
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    if(newImage == nil){
+        NSLog(@"scale image fail");
+    }
+    UIGraphicsEndImageContext();
+    NSData *newImageData = UIImageJPEGRepresentation(newImage, 1.0);
+    return newImageData;
+}
+
 @end
