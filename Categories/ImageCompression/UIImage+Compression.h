6f438a079b3f93389db65793c67e661b19170d8b
Murphy Zheng <murphy@mieasy.com>
2017-09-07 15:41:27 +0800

    Add Image Compression Category

---
+++ Categories/ImageCompression/UIImage+Compression.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Compression.h
+//  YMCategory
+//
+//  Created by Murphy Zheng on 17/9/7.
+//  Copyright © 2017年 mieasy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Compression)
+
+@end
---
+++ Categories/ImageCompression/UIImage+Compression.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Compression.m
+//  YMCategory
+//
+//  Created by Murphy Zheng on 17/9/7.
+//  Copyright © 2017年 mieasy. All rights reserved.
+//
+
+#import "UIImage+Compression.h"
+
+@implementation UIImage (Compression)
+
+@end
